22df8bd968d87ec0e78c4c5272cb19f073970ce2
05ryder <jayrydercuch@gmail.com>
2025-08-08 10:01:26 -0400

    Fix main app configuration for Railway deployment - Phase 10 migration

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'tinymce',  # Temporarily disabled
-    'main',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
